1ebbc28049e7efe52bff25fc02675ccd1b72e60c
Yangqi Su <yqsu95@gmail.com>
2025-09-11 17:28:35 -0400

    refactor plotting: - umap plots of test data is now handled via multiprocessing for efficient training

--- perttf/model/train_function.py
+++ perttf/model/train_function.py
@@ -14,7 +14,7 @@ from torch.utils.data import Dataset, DataLoader
 
 from anndata import AnnData
 import scanpy as sc
-
+import multiprocessing
 import wandb
 from scipy.sparse import issparse
 
@@ -31,7 +31,7 @@ import matplotlib.pyplot as plt
 
 from perttf.model.train_data_gen import prepare_data,prepare_dataloader
 from perttf.utils.set_optimizer import create_optimizer_dict
-
+from perttf.utils.plot import process_and_log_umaps
 def train(model: nn.Module,
           loader: DataLoader,
           config,
@@ -495,7 +495,6 @@ def evaluate(model: nn.Module,
 
     return total_loss / total_num, total_loss_next / total_num, total_error / total_num, total_error_next / total_num, total_dab / total_num, total_cls / total_num, total_pert / total_num, total_ps / total_num, total_ps_next / total_num
 
-
 def eval_testdata(
     model: nn.Module,
     adata_t: AnnData,
@@ -510,8 +509,11 @@ def eval_testdata(
     eval_key = "", # titles for evaluation
     make_plots = True,
     predict_expr = False
-) -> Optional[Dict]:
-    """evaluate the model on test dataset of adata_t"""
+) -> Optional[Dict]: # Returns a dictionary containing the AnnData object
+    """
+    Evaluate the model on test data and return an AnnData object with embeddings.
+    Plotting and UMAP are offloaded to a separate process.
+    """
     model.eval()
 
     # copy adata_t to avoid reuse previously computed results stored in adata_t
@@ -695,152 +697,8 @@ def eval_testdata(
         index_to_celltype = {v: k for k, v in cell_type_to_index.items()}
         predicted_celltypes = [index_to_celltype[i] for i in label_predictions_cls]
         adata_t.obs['predicted_celltype'] = predicted_celltypes
+    return adata_t
 
-        results = {}
-        #try:
-        #    results = eval_scib_metrics(adata_t)
-        #        #batch_key = 'str_batch' if config.DSBN else None,
-        #        #label_key = 'celltype' if config.cell_type_classifier else None
-        #except Exception as e:
-        #    traceback.print_exc()
-        #    logger.error(e)
-        if make_plots:
-            if next_cell_prediction:
-                sc.pp.neighbors(adata_t, use_rep="X_scGPT_next")
-                sc.tl.umap(adata_t, min_dist=0.3)
-                if config.cell_type_classifier:
-                    fign1 = sc.pl.umap(adata_t, color=["celltype"],
-                        title=[f"{eval_key} celltype, e{epoch}, pred embedding",],
-                        frameon=False,
-                        return_fig=True,
-                        show=False,
-                    )
-                    plt.close()
-                    results["next_umap_celltype"] = fign1
-                if config.perturbation_classifier_weight > -1:
-                    fign2 = sc.pl.umap(adata_t,color=["genotype"],
-                        title=[f"{eval_key} genotype, e{epoch}, pred embedding",],
-                        frameon=False,
-                        return_fig=True,
-                        show=False,
-                    )
-                    plt.close()
-                    results["next_umap_genotype"] = fign2
-                    fign3 = sc.pl.umap(adata_t,color=["genotype_next"],
-                        title=[f"{eval_key} next genotype, e{epoch}, pred embedding",],
-                        frameon=False,
-                        return_fig=True,
-                        show=False,
-                        #palette="Set1",
-                    )
-                    plt.close()
-                    results["next_umap_genotype_next"] = fign3
-
-            # all other evaluations
-            sc.pp.neighbors(adata_t, use_rep="X_scGPT")
-            sc.tl.umap(adata_t, min_dist=0.3)
-
-            if "batch" in adata_t.obs:
-                fig = sc.pl.umap(
-                    adata_t,
-                    color=["batch"],
-                    title=[f"{eval_key} batch, e{epoch}"],
-                    frameon=False,
-                    return_fig=True,
-                    show=False,
-                )
-                plt.close()
-                results["batch_umap"] = fig
-
-            #sc.pp.neighbors(adata_t, use_rep="X_scGPT")
-            #fig = sc.tl.umap(adata_t, min_dist=0.3)
-            #results["umap_X_scGPT"] = fig
-
-            if config.cell_type_classifier:
-                fig = sc.pl.umap(
-                    adata_t,
-                    color=["celltype"],
-                    title=[
-                        f"{eval_key} celltype, e{epoch}",
-                    ],
-                    frameon=False,
-                    return_fig=True,
-                    show=False,
-                )
-                plt.close()
-                results["celltype_umap"] = fig
-                fig4 = sc.pl.umap(
-                    adata_t,
-                    color=["predicted_celltype"],
-                    title=[
-                        f"{eval_key} pred celltype, e{epoch}",
-                    ],
-                    frameon=False,
-                    return_fig=True,
-                    show=False,
-                    #palette="Set1",
-                )
-                plt.close()
-                results["pred_celltype"] = fig4
-
-            if config.perturbation_classifier_weight > -1:
-                fig = sc.pl.umap(
-                    adata_t,
-                    color=["genotype"],
-                    title=[
-                        f"{eval_key} genotype, e{epoch}",
-                    ],
-                    frameon=False,
-                    return_fig=True,
-                    show=False,
-                )
-                plt.close()
-                results["genotype_umap"] = fig
-                fig2 = sc.pl.umap(
-                    adata_t,
-                    color=["genotype"],
-                    title=[
-                        f"{eval_key} genotype e{epoch}",
-                    ],
-                    frameon=False,
-                    return_fig=True,
-                    show=False,
-                    palette="tab20",
-                )
-                plt.close()
-                results["genotype_umap2"] = fig2
-
-                fig3 = sc.pl.umap(
-                    adata_t,
-                    color=["predicted_genotype"],
-                    title=[
-                        f"{eval_key} pred genotype, with different color e{epoch}",
-                    ],
-                    frameon=False,
-                    return_fig=True,
-                    show=False,
-                    #palette="Set1",
-                )
-                plt.close()
-                results["pred_genotype"] = fig3
-
-                if "genotype_next" in adata_t.obs:
-                    fig5 = sc.pl.umap(
-                        adata_t,
-                        color=["genotype_next"],
-                        title=[
-                            f"{eval_key} next genotype, with different color e{epoch}",
-                        ],
-                        frameon=False,
-                        return_fig=True,
-                        show=False,
-                        #palette="Set1",
-                    )
-                    plt.close()
-                    results["genotype_next"] = fig5
-
-    results['adata'] = adata_t
-    return results
 
 def wrapper_train(model, config, data_gen,
                   logger = scg.logger,
@@ -880,9 +738,18 @@ def wrapper_train(model, config, data_gen,
     # later, use the following to load json file
     #config_data = json.load(open(save_dir / 'config.json', 'r'))
     train_loader, valid_loader = data_gen['train_loader'], data_gen['valid_loader']
+    evaltest_processes = []
+
     for epoch in range(1, config.epochs + 1):
         epoch_start_time = time.time()
-
+        remaining_processes = []
+        for p in evaltest_processes:
+            if p.is_alive():
+                remaining_processes.append(p)
+            else:
+                p.join()  # Joins the process to release resources
+        evaltest_processes = remaining_processes
+        logger.info(f"Active UMAP processes: {len( evaltest_processes)}")
 
         if config.do_train:
             train(
@@ -923,18 +790,15 @@ def wrapper_train(model, config, data_gen,
                 logger.info(f"Best model with score {best_val_loss:5.4f}")
 
         #if epoch % config.save_eval_interval == 0 or epoch == config.epochs:
-
         if epoch % 2 == 1:
-            if logger is not None:
-                logger.info(f"Saving model to {save_dir}")
+            logger.info(f"Saving model to {save_dir}")
             torch.save(best_model.state_dict(), save_dir / f"model_e{epoch}.pt")
-            # change images of each epoch to subfolder
-            save_dir2=save_dir / f'e{epoch}_imgs'
+            save_dir2 = save_dir / f'e{epoch}_imgs'
             save_dir2.mkdir(parents=True, exist_ok=True)
-            # eval on testdata
-            metrics_to_log={}
+            
             for eval_dict_key, eval_adata in eval_adata_dict.items():
-                results = eval_testdata(
+                # Step 1: Get AnnData with embeddings from the main process
+                results = multi_process_eval_testdata(
                     #best_model,
                     model, # use current model
                     adata_t = eval_adata, #adata_t=data_gen['adata_sorted'], # if config.per_seq_batch_sample else adata,
@@ -946,56 +810,29 @@ def wrapper_train(model, config, data_gen,
                     epoch=epoch,
                     eval_key=eval_dict_key,
                 )
+                adata_with_embeddings = results
+                
+                # Step 2: Save the data to a temporary file for the child process
+                #temp_adata_path = save_dir2 / f"temp_adata_{eval_dict_key}_e{epoch}.h5ad"
+                #adata_with_embeddings.write_h5ad(temp_adata_path)
 
-                # metrics_to_log = {"test/" + k: v for k, v in results.items() if k != "adata"}
-                #if "umap" in results:
-                #    results["umap"].savefig(save_dir / f"embeddings_umap[cls]_e{best_model_epoch}.png", dpi=300,bbox_inches='tight')
+                # Step 3: Create and start the background process
+                logger.info(f"Starting background process for UMAP on epoch {epoch} for '{eval_dict_key}'")
+                
+                # Pass data_gen['ps_names'] if it exists, otherwise None
+                ps_names = data_gen.get('ps_names', None)
+                
+                p = multiprocessing.Process(
+                    target=process_and_log_umaps,
+                    args=(adata_with_embeddings, config, epoch, eval_dict_key, save_dir2, ps_names)
+                )
+                p.start()
+                evaltest_processes.append(p)
 
-                save_image_types=["batch_umap","celltype_umap","genotype_umap",
-                    "genotype_umap2","pred_genotype",
-                    "pred_celltype","genotype_next",
-                    "next_umap_celltype","next_umap_genotype","next_umap_genotype_next"]
-                for res_key, res_img_val in results.items():
-                    if res_key in save_image_types:
-                        res_img_val.savefig(save_dir2 / f"{eval_dict_key}_embeddings_{res_key}_e{epoch}.png", dpi=300,bbox_inches='tight')
-                        metrics_to_log[f"test/{eval_dict_key}_{res_key}"] = wandb.Image(
-                            str(save_dir2 / f"{eval_dict_key}_embeddings_{res_key}_e{epoch}.png"),
-                            caption=f"{eval_dict_key}_{res_key} epoch {epoch}",
-                        )
-                # save the PS calculations
-                if config.ps_weight > 0:
-                    # plot the existing loness columns
-                    adata_ret = results["adata"]
-                    loness_columns = [x for x in adata_ret.obs if x.startswith('lonESS')]
-                    for lon_c in loness_columns:
-                        fig_lonc = sc.pl.umap(adata_ret,color=[lon_c],title=[f"loness {lon_c}  e{epoch}",],
-                            frameon=False,return_fig=True, show=False,palette="tab20",)
-                        plt.close()
-                        # Replace '/' with '_' in ps_names
-                        lon_c_rep=lon_c.replace('/', '_')
-                        fig_lonc.savefig(save_dir2 / f"{eval_dict_key}_loness_{lon_c_rep}_e{epoch}.png", dpi=300,bbox_inches='tight')
-                    if ('ps_names' in data_gen) & ('ps_pred' in adata_ret.obsm) :
-                        predicted_ps_names = data_gen['ps_names']
-                        predicted_ps_score = adata_ret.obsm['ps_pred']
-                        logger.info(f"predicted_ps_names: {predicted_ps_names}")
-                        logger.info(f"predicted_ps_score: {predicted_ps_score.shape}")
-                        for si_i in range(len(predicted_ps_names)):
-                            lon_c = predicted_ps_names[si_i]
-                            lon_c_rep=lon_c.replace('/', '_')
-                            adata_ret.obs[f'{lon_c_rep}_pred'] = predicted_ps_score[:,min(si_i, predicted_ps_score.shape[1]-1)]
-                            fig_lonc_pred = sc.pl.umap(adata_ret,color=[f'{lon_c_rep}_pred'],title=[f"loness {lon_c_rep}_pred  e{epoch}",],
-                                frameon=False,return_fig=True, show=False,palette="tab20",)
-                            plt.close()
-                            fig_lonc_pred.savefig(save_dir2 / f"{eval_dict_key}_loness_{lon_c_rep}_pred_e{epoch}.png", dpi=300,bbox_inches='tight')
-                    results["adata"] = adata_ret
-                if "adata" in results:
-                    results["adata"].write_h5ad(save_dir / f'adata_last_validation_{eval_dict_key}.h5ad')
+        
 
-            #if "adata" in results_p:
-            #    results_p["adata"].write_h5ad(save_dir / f'adata_last_validation.h5ad')
-
-            metrics_to_log["test/best_model_epoch"] = best_model_epoch
-            wandb.log(metrics_to_log)
+            #metrics_to_log["test/best_model_epoch"] = best_model_epoch
+            wandb.log({"test/best_model_epoch":best_model_epoch})
             # wandb.log({"avg_bio": results.get("avg_bio", 0.0)})
 
         optimizer_dict['scheduler'].step()
