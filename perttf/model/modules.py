- pass random state from seed variable to datamanagers - use elu instead of linear (need to change this to settable) - changed softmax expression activation (might remove softmax altogethor) - fix tokenize_batch cls_id default to 1 isntead of the cls token - add gene sampling and independent mvc tokens to eval_testdata

perttf/model/modules.py
@@ -59,7 +59,7 @@ class ExpressionActivate(nn.Module):
 
             # 2. Apply a non-negative activation to the energy logit.
             # Softplus ensures the magnitude component is always positive.
-            activated_energy = F.softplus(energy_logit)+1000
+            activated_energy = F.softplus(energy_logit)
 
             # 3. Apply softmax to the rest of the vector to get a probability distribution.
             distribution = F.softmax(distribution_logits, dim=-1) 

perttf/model/pertTF.py
@@ -189,7 +189,7 @@ class PerturbationTFModel(TransformerModel):
         self.pert_exp_mode = kwargs.pop("pert_exp_mode", 'concat')
         self.pert_exp_mode = self.pert_exp_mode if self.pert_exp_mode in ['concat', 'sum', 'direct_sum'] else 'concat'
         super().__init__(*args, **kwargs)
-        self.expr_act = ExpressionActivate(activation = 'linear')
+        self.expr_act = ExpressionActivate(activation = 'elu')
         # add perturbation encoder
         # variables are defined in super class
         d_model = self.d_model
@@ -514,19 +514,20 @@ class PerturbationTFModel(TransformerModel):
 
     def encode_batch_with_perturb(
         self,
-        src: Tensor,
-        values: Tensor,
-        src_key_padding_mask: Tensor,
-        batch_size: int,
-        batch_labels: Optional[Tensor] = None,
+        src: Tensor, # gene_ids matching values
+        values: Tensor, # values of expression
+        src_key_padding_mask: Tensor, # padding 
+        batch_size: int, # batch size to run inference
+        batch_labels: Optional[Tensor] = None, 
         pert_labels: Optional[Tensor] = None, # the current perturbation label
         pert_labels_next: Optional[Tensor] = None, # the next perturbation label
         perturbation: Optional[Tensor] = None, # the perturbation induced
-        pert_scale: Optional[Tensor] = None,
+        pert_scale: Optional[Tensor] = None, # scale multiplier for perturbation 
         output_to_cpu: bool = True,
         time_step: Optional[int] = None,
         return_np: bool = False,
-        predict_expr = False
+        predict_expr = False,
+        mvc_src: Tensor = None # optional MVC tensor of gene ids for MVC decoder
     ) -> Tuple[Tensor, Tensor, Tensor]:
         """
         revised scgpt.TransformerModel.encode_batch but with additional perturbation
@@ -578,10 +579,11 @@ class PerturbationTFModel(TransformerModel):
         
         expr_dict = {}
         if predict_expr:
-            shape_expr = (N, src.size(1))
-            mlm_outputs, mlm_zero_outputs = array_func(shape_expr, dtype=float32_), array_func(shape_expr, dtype=float32_)
-            mvc_outputs, mvc_zero_outputs = array_func(shape_expr, dtype=float32_), array_func(shape_expr, dtype=float32_)
-            mvc_next_outputs, mvc_next_zero_outputs = array_func(shape_expr, dtype=float32_), array_func(shape_expr, dtype=float32_)
+            mlm_expr_shape = (N, src.size(1)) 
+            mvc_expr_shape = (N, src.size(1)) if mvc_src is None else (N, mvc_src.size(1))
+            mlm_outputs, mlm_zero_outputs = array_func(mlm_expr_shape, dtype=float32_), array_func(mlm_expr_shape, dtype=float32_)
+            mvc_outputs, mvc_zero_outputs = array_func(mvc_expr_shape, dtype=float32_), array_func(mvc_expr_shape, dtype=float32_)
+            mvc_next_outputs, mvc_next_zero_outputs = array_func(mvc_expr_shape, dtype=float32_), array_func(mvc_expr_shape, dtype=float32_)
 
         for i in trange(0, N, batch_size):
             src_d = src[i : i + batch_size].to(device)
@@ -592,6 +594,7 @@ class PerturbationTFModel(TransformerModel):
             pert_labels_next_d = pert_labels_next[i : i + batch_size].to(device) if pert_labels_next is not None else None
             perturbation_d = perturbation[i : i + batch_size].to(device) if perturbation is not None else None
             pert_scale_d = pert_scale[i : i + batch_size].to(device) if pert_scale is not None else None
+            mvc_src_d = mvc_src[i:i+batch_size].to(device) if mvc_src is not None else None
             raw_output = self._encode(
                 src_d,
                 values_d,
@@ -677,22 +680,23 @@ class PerturbationTFModel(TransformerModel):
                 ),
                 # else transformer_output + batch_emb.unsqueeze(1),
                 )
+                cur_gene_token_embs = self.encoder(mvc_src_d) if mvc_src_d is not None else self.cur_gene_token_embs
                 mvc_output = self.mvc_decoder(
                                 cell_emb if not self.use_batch_labels
                                 else torch.cat([cell_emb, batch_emb], 
                                 dim=1
                                 ), # else cell_emb + batch_emb,
-                            self.cur_gene_token_embs,)
+                            cur_gene_token_embs,)
                 if pert_labels_next_d is not None:
                     mvc_output_next = self.mvc_decoder(
                                 cell_emb_next if not self.use_batch_labels
                                 else torch.cat([cell_emb_next, batch_emb], 
                                 dim=1
                                 ), # else cell_emb + batch_emb,
-                            self.cur_gene_token_embs,)
+                            cur_gene_token_embs,)
                 else:
                     mvc_output_next = mvc_output
-
+                
                 mlm_pred, mlm_zero_probs =  self.expr_act(mlm_output['pred']), mlm_output['zero_probs'] if self.explicit_zero_prob else 1
                 mvc_pred, mvc_zero_probs =  self.expr_act(mvc_output['pred']), mvc_output['zero_probs'] if self.explicit_zero_prob else 1
                 mvc_pred_next, mvc_zero_probs_next =  self.expr_act(mvc_output_next['pred']), mvc_output_next['zero_probs'] if self.explicit_zero_prob else 1

perttf/model/train_function.py
@@ -699,6 +699,7 @@ def eval_testdata(
                         else:
                             perturbation_labels_next.append(curr)
                 pert_scale = np.array([np.array([-1.0]) if perturbed and adata_t.obs.genotype.iloc[i] != 'WT' else np.array([1.0]) for i, perturbed in enumerate(adata_t.obs.genotype != adata_t.obs.genotype_next)])
+    
     next_genotype = np.array([genotype_to_index[genotype] for genotype in adata_t.obs["genotype_next"].tolist()])
     if config.next_cell_pred_type ==  'lochness':
         if hasattr(config, "pred_lochness_next") and config.pred_lochness_next >0:
@@ -723,7 +724,13 @@ def eval_testdata(
 
     batch_ids = np.array(batch_ids)
 
-
+    if not config.simple_sampling:
+        max_seq_len = 10000
+        cls_gene_ids = np.insert(gene_ids, 0, vocab[config.cls_token]) # default should always be to insert a cls token at the front
+        full_gene_ids = torch.stack([torch.from_numpy(cls_gene_ids).long() for i in range(adata_t.shape[0])], dim = 0)
+    else:
+        max_seq_len = config.max_seq_len
+        full_gene_ids = None
     # Evaluate cls cell embeddings
     if "cls" in include_types:
         if logger is not None:
@@ -731,12 +738,15 @@ def eval_testdata(
         tokenized_all, gene_idx_list= tokenize_and_pad_batch(
             all_counts,
             gene_ids,
-            max_len=config.max_seq_len,
+            max_len=max_seq_len,
             vocab=vocab,
             pad_token=config.pad_token,
             pad_value=config.pad_value,
             append_cls=True,  # append <cls> token at the beginning
             include_zero_gene=True,
+            simple_sampling = config.simple_sampling,
+            nonzero_prop = config.nonzero_prop,
+            fix_nonzero_prop =  config.fix_nonzero_prop
         )
 
 
@@ -754,13 +764,16 @@ def eval_testdata(
             tokenized_all_next, _ = tokenize_and_pad_batch(
                 all_counts_next,
                 gene_ids,
-                max_len=config.max_seq_len,
+                max_len=max_seq_len,
                 vocab=vocab,
                 pad_token=config.pad_token,
                 pad_value=config.pad_value,
                 append_cls=True,  # append <cls> token at the beginning
                 include_zero_gene=True,
-                sample_indices=gene_idx_list
+                sample_indices=gene_idx_list,
+                simple_sampling = config.simple_sampling,
+                nonzero_prop = config.nonzero_prop,
+                fix_nonzero_prop =  config.fix_nonzero_prop
             )
             all_gene_ids_next, all_values_next = tokenized_all_next["genes"], tokenized_all_next["values"]
         
@@ -784,7 +797,8 @@ def eval_testdata(
                 pert_scale = torch.from_numpy(pert_scale).float() if reciprical_sampling else None,
                 time_step=0,
                 return_np=True,
-                predict_expr = predict_expr
+                predict_expr = predict_expr,
+                mvc_src = full_gene_ids
             )
 
         cell_embeddings = cell_embeddings / np.linalg.norm(

perttf/utils/custom_tokenizer.py
@@ -122,7 +122,7 @@ def tokenize_batch(
     return_pt: bool = True,
     append_cls: bool = True,
     include_zero_gene: bool = False,
-    cls_id: int = "<cls>",
+    cls_id: int = 1,
     cls_value: int = -3,
     mod_type: np.ndarray = None,
     cls_id_mod_type: int = None,