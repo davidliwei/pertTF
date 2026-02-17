- fix size factor scaling - option to not use size factor scaling with `sf_scaling` parameter to model __init__ - use input size factors only during training prevent info leakage

perttf/model/base_model.py
@@ -54,6 +54,7 @@ class BaseModel(nn.Module):
         fast_transformer_backend: str = "flash",
         pre_norm: bool = False,
         distribution: str = None,
+        sf_scaling: bool = True,
         **kwargs
     ):
         super().__init__()
@@ -80,6 +81,7 @@ class BaseModel(nn.Module):
         self.use_fast_transformer = False if not torch.cuda.is_available() or fast_transformer_backend == 'vanilla' else use_fast_transformer
         self.fast_transformer_backend = fast_transformer_backend if torch.cuda.is_available() else 'vanilla'
         self.distribution = distribution
+        self.sf_scaling = sf_scaling
         if self.input_emb_style not in ["category", "continuous", "scaling",'autobin']:
             raise ValueError(
                 f"input_emb_style should be one of category, continuous, scaling, "
@@ -137,7 +139,8 @@ class BaseModel(nn.Module):
                 arch_style=self.mvc_decoder_style,
                 explicit_zero_prob=self.explicit_zero_prob,
                 use_batch_labels=self.use_batch_labels,
-                distribution=self.distribution
+                distribution=self.distribution,
+                sf_scaling = self.sf_scaling
             )
 
         if do_dab:

perttf/model/modules.py
@@ -877,6 +877,7 @@ class MVCDecoder(nn.Module):
         explicit_zero_prob: bool = True, 
         distribution: str = None,  # Options: 'nb', 'zinb', 'hnb', 'zig', 'pois', 'zipois'
         use_batch_labels: bool = False,
+        sf_scaling: bool = True,
     ) -> None:
         """
         Args:
@@ -899,7 +900,7 @@ class MVCDecoder(nn.Module):
         """
         super().__init__()
         self.distribution = None if distribution is None else distribution.lower()
-        
+        self.sf_scaling = sf_scaling
         valid_dists = ['nb', 'zinb', 'hnb', 'zig', 'pois', 'zipois', None]
         if self.distribution not in valid_dists:
             raise ValueError(f"Unknown distribution: {self.distribution}")
@@ -1005,8 +1006,11 @@ class MVCDecoder(nn.Module):
         if self.distribution == 'zig':
             mu = pred_concentration
         else:
-            target_size_factor = 1 if target_size_factor is None else target_size_factor
-            mu = pred_concentration * target_size_factor
+            # maybe use constant sizefactor (sf_scaling = False) and let the model learn what it can
+            target_size_factor = 1 if target_size_factor is None or not self.sf_scaling else target_size_factor
+            # OR this should be division because inputs are size factor agnostic thus outputs are size factor agnostic??
+            # Furthermore, testing shows lower NLL when division is used as opposed to multiplication
+            mu = pred_concentration / target_size_factor 
 
         return {
             "pred": mu,